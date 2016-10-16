08d4f7e9d5ed1ecdea390be53e93d2e1f327c26a
Leran Firer <leranf@gmail.com>
2016-10-16 14:08:12 -0700

    cleans up server file

--- server.js
+++ server.js
@@ -2,13 +2,9 @@ const express = require('express');
 const app = express();
 
 // dotenv sets up the process.env variables. Put env variables in a .env file in your root
-require('dotenv').config({ path: `${__dirname}/../.env` });
+require('dotenv').config({ path: `${__dirname}/./.env` });
 const PORT = process.env.PORT;
-const passport = require('passport');
-const Strategy = require('passport-facebook').Strategy;
 const db = require('./config/db.js');
-const User = require('./users/users');
-const Vote = require('./votes/votes');
 
 require('./config/middleware.js')(app, express);
 

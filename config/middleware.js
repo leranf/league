e1e9f77272cd8a5d97fd3fa0ddbde4ca27ac67b6
Leran Firer <leranf@gmail.com>
2016-10-16 14:05:24 -0700

    adds session middleware

--- config/middleware.js
+++ config/middleware.js
@@ -1,8 +1,15 @@
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const session = require('client-sessions');
 
 module.exports = (app, express) => {
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
+  app.use(session({
+    cookieName: 'session',
+    secret: 'eg[isfd-8yF9-7w2315df{}+Ijsli;;to8',
+    duration: 30 * 60 * 1000,
+    activeDuration: 5 * 60 * 1000,
+  }));
 };

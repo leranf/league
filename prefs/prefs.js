be69c435af190e52c940675380f89652f9355a97
Leran Firer <leranf@gmail.com>
2016-10-16 14:07:51 -0700

    changes some properties to integers for comparison instead of strings

--- prefs/prefs.js
+++ prefs/prefs.js
@@ -1,17 +1,17 @@
+'use strict';
 const db = require('../config/db');
 const jwt = require('jwt-simple');
 const Sequelize = require('sequelize');
 
 const Pref = db.define('prefs', {
-  minAge: Sequelize.STRING,
-  maxAge: Sequelize.STRING,
-  distance: Sequelize.STRING,
+  userid: Sequelize.INTEGER,
+  minage: Sequelize.INTEGER,
+  maxage: Sequelize.INTEGER,
+  distance: Sequelize.INTEGER,
   gender: Sequelize.STRING,
   religion: Sequelize.STRING,
 });
 
-// creates userId column in prefs table
-Pref.belongsTo(User, { foreignKey: 'userId', constraints: false });
-
 Pref.sync();
-User.sync();
+
+module.exports = Pref;

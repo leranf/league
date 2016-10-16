17fc55982c2f4bfc11ce5cb6ff1404cdc6340e68
Leran Firer <leranf@gmail.com>
2016-10-16 14:10:54 -0700

    creates userr session variable upon signUp and login, looks for user info to find matches

--- users/usersController.js
+++ users/usersController.js
@@ -1,5 +1,5 @@
 const User = require('./users');
-const helpers = require('../config/helpers');
+const session = require('client-sessions');
 
 module.exports = {
 
@@ -7,34 +7,60 @@ module.exports = {
     const username = req.body.username;
     const password = req.body.password;
     User.login(username, password)
-    .then(match => {
-      console.log(match);
-      // res.json(!!match);
+    .then(user => {
+      console.log('logged in bad password', user);
+      if (user) {
+        req.session.user = user;
+        res.json('sucess logging in');
+        // res.redirect('/home');
+      } else {
+        res.json('Invalid username or password');
+      }
     })
     .catch(err => {
-      console.error(err);
+      res.json('There was an error logging in. Please try a different username or password');
     });
   },
 
   signUp: (req, res, next) => {
     User.signUp(req.body)
-    .then(match => {
-      console.log(match);
-      // res.json(!!match);
+    .then(newUser => {
+      console.log('user', newUser);
+      if (typeof newUser === 'object') {
+        req.session.user = newUser;
+        res.json('success!!');
+        // res.redirect('/home');
+      } else {
+        res.json(newUser);
+      }
     })
     .catch(err => {
       console.error(err);
+      res.json('Error signing up. Please try a different username or password');
     });
   },
 
   findMatches: (req, res, next) => {
-    User.findMatches(req.user)
-    .then(matches => {
-      res.json(matches);
-    })
-    .catch(err => {
-      console.error(err)
-    });
+    if (req.session && req.session.user) {
+      User.findOne({ where: { username: req.session.user.username} })
+      .then(user => {
+        if (user) {
+          // console.log('user in findMatches controller', user.dataValues);
+          User.findMatches(user.dataValues)
+          .then(matches => {
+            res.json(matches);
+          });
+        } else {
+          res.redirect('/login');
+        }
+      })
+      .catch(err => {
+        console.error(err)
+      });
+    } else {
+      res.redirect('/login');
+    }
+
   },
 
 };
